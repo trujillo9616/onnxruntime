f6d931ffec5e068f62e67e97a15af4afbfdb4f0a
Adrian Trujillo Duron <54916945+trujillo9616@users.noreply.github.com>
2020-06-01 03:43:28 -0500

    Update build.py

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -522,9 +522,11 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home,
         "-Donnxruntime_BUILD_WINML_TESTS=" + (
             "OFF" if args.skip_winml_tests else "ON"),
         "-Donnxruntime_GENERATE_TEST_REPORTS=ON",
+        """
         "-Donnxruntime_DEV_MODE=" + (
             "OFF" if args.android or args.use_acl or
             (args.ios and is_macOS()) else "ON"),
+        """
         "-DPYTHON_EXECUTABLE=" + sys.executable,
         "-Donnxruntime_USE_CUDA=" + ("ON" if args.use_cuda else "OFF"),
         "-Donnxruntime_CUDNN_HOME=" + (cudnn_home if args.use_cuda else ""),
